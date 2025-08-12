bbb4742c370d0a2bfce6bca457108d3c2c7c8b84
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-12 15:24:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./styles/styles.css";
 
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
