285a31cc8ca61e6c3bedae6b1f6cd57400995f9a
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-13 12:05:16 +0530

    Update calendarUtils.js

--- src/components/calendarUtils.js
+++ src/components/calendarUtils.js
@@ -1,1 +1,19 @@
+import moment from 'moment';
+import { momentLocalizer } from 'react-big-calendar';
 
+export const localizer = momentLocalizer(moment);
+
+export const eventStyleGetter = (event) => {
+  const isPast = moment(event.start).isBefore(moment(), 'day');
+  return {
+    style: {
+      backgroundColor: isPast ? 'rgb(222, 105, 135)' : 'rgb(140, 189, 76)',
+      borderRadius: '4px',
+      opacity: 0.9,
+      color: 'white',
+      border: '0px',
+      display: 'block',
+      fontSize: '0.9rem'
+    }
+  };
+};
