c3f465b26d626a487755035fdb3aa3bf801e8c40
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-13 12:59:36 +0530

    Update FilterButtons.js

--- src/components/FilterButtons.js
+++ src/components/FilterButtons.js
@@ -1,1 +1,19 @@
+import React from "react";
 
+function FilterButtons({ filter, setFilter }) {
+  return (
+    <div style={{ marginBottom: 20 }}>
+      <button className="btn" onClick={() => setFilter("all")} style={{ fontWeight: filter === "all" ? "bold" : "normal" }}>
+        All
+      </button>
+      <button className="btn" onClick={() => setFilter("past")} style={{ fontWeight: filter === "past" ? "bold" : "normal" }}>
+        Past
+      </button>
+      <button className="btn" onClick={() => setFilter("upcoming")} style={{ fontWeight: filter === "upcoming" ? "bold" : "normal" }}>
+        Upcoming
+      </button>
+    </div>
+  );
+}
+
+export default FilterButtons;
