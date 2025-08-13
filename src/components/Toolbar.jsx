a1a13e97bfd4beb95acfd190cc1c54e7238ca6ae
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-13 11:43:51 +0530

    Update Toolbar.jsx

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
+const Toolbar = ({ setFilter }) => (
+  <div className="toolbar">
+    <button className="btn" onClick={() => setFilter('all')}>All</button>
+    <button className="btn" onClick={() => setFilter('past')}>Past</button>
+    <button className="btn" onClick={() => setFilter('upcoming')}>Upcoming</button>
+  </div>
+);
+
+Toolbar.propTypes = {
+  setFilter: PropTypes.func.isRequired
+};
+
+export default Toolbar;
